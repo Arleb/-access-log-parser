23de76b8bbdfd0f7e5fc23e7b5e3c7d043607338
Anton Lebedev <git.user.lebedevar2013@yandex.ru>
2023-09-12 17:29:14 +0300

    простой кальулятор

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+       System.out.println("Введите первое число:");
+       int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        //сумма чисел
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма чисел: "+ sum);
+
+        //разность чисел
+        int diff = firstNumber - secondNumber;
+        System.out.println("Разность чисел: "+ diff);
+
+        //произведение чисел
+        int prod = firstNumber * secondNumber;
+        System.out.println("Произведение чисел: "+ prod);
+
+        //частное чисел
+        double part = (double) firstNumber / secondNumber;
+        System.out.println("Частное чисел : "+ part);
+
     }
 }
