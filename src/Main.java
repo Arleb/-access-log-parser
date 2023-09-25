e8122a639b0f87254963e7b8251bdb8c7ffd0b52
Anton Lebedev <git.user.lebedevar2013@yandex.ru>
2023-09-25 20:22:08 +0300

    Написан бесконечный цикл

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+    public static void main (String[] args){
+        int count = 0;
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите путь к файлу:");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            if (!fileExists || file.isDirectory()) {
+                System.out.println("Указанный файл не существует или путь ведет к папке");
+                continue;
+            }
+            count++;
+            System.out.println("Путь указан верно");
+            System.out.println("Этот файл номер " + count);
+        }
     }
 }
